4f8acbeeb4079d4f0cdcb034c7909627c258a418
RulonRide67 <armismadiar20@gmail.com>
2025-04-14 22:09:00 +0500

    Update MyStack.java

--- Assignment2ads/src/MyStack.java
+++ Assignment2ads/src/MyStack.java
@@ -5,11 +5,13 @@ public class MyStack<T> {
         list.add(element);
     }
 
-//    public T pop() {
-//        if (isEmpty())
-//            throw new RuntimeException("Stack is empty");
-//        return list.remove(list.size() - 1);
-//    }
+   public T pop() {
+       if (isEmpty())
+           throw new RuntimeException("Stack is empty");
+       T removedEl = list.get(list.size() - 1)
+       list.remove(list.size() - 1)
+       return removedEL;
+   }
 
     public T peek() {
         if (isEmpty())
