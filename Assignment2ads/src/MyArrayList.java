11561b78b281bf9505422d2fbb2118b1c4b784a5
RulonRide67 <armismadiar20@gmail.com>
2025-04-18 10:03:25 +0500

    Update MyArrayList.java

--- Assignment2ads/src/MyArrayList.java
+++ Assignment2ads/src/MyArrayList.java
@@ -1,3 +1,7 @@
+import com.sun.jdi.ObjectCollectedException;
+
+import java.util.Iterator;
+
 public class MyArrayList<T> implements MyList<T> {
     private Object[] elements;
     private int size;
@@ -33,9 +37,74 @@ public class MyArrayList<T> implements MyList<T> {
         size--;
     }
 
+    @Override
+    public void removeFirst() {
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        remove(size - 1);
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (object.equals(elements[i])) {
+                return i;
+            }else throw new ObjectCollectedException("Object not found");
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        if (isEmpty())
+            throw new IndexOutOfBoundsException("The list is empty");
+        for (int i = size; i > -1; i--) {
+            if (object.equals(elements[i])) {
+                return i;
+            }else throw new ObjectCollectedException("Object not found");
+        }
+        return 0;
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    
+    @Override
+    public boolean exists(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (object.equals(elements[i])) {
+                return true;
+            }else return false;
+        }
+        return size == 0;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
     public T get(int index) {
         checkIndex(index);
         return (T) elements[index];
+    }
+
+    @Override
+    public T getFirst() {
+        return get(0);
+    }
+
+    @Override
+    public T getLast() {
+        return null;
     }
 
     public int size() {
@@ -52,5 +121,37 @@ public class MyArrayList<T> implements MyList<T> {
     public void set(int index, T newElement) {
         checkIndex(index);
         elements[index] = newElement;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        if (size == elements.length) {
+            capinc(elements);
+        }
+        for (int i = size; i == index ; i--) {
+            elements[i+1] = elements[i];
+        }
+        elements[index] = element;
+    }
+
+    @Override
+    public void addFirst(T element) {
+        if (size == elements.length) {
+            capinc(elements);
+        }
+        for (int i = size; i > 0; i--) {
+            elements[i - 1] = elements[i];
+        }
+        elements[0] = element;
+    }
+
+    @Override
+    public void addLast(T element) {
+        add(element);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
     }
 }
