7ea702b6af0045208fb1c3401b9abbc96977ec3d
RulonRide67 <armismadiar20@gmail.com>
2025-04-18 09:53:08 +0500

    Update MyLinkedList.java

--- Assignment2ads/src/MyLinkedList.java
+++ Assignment2ads/src/MyLinkedList.java
@@ -1,4 +1,11 @@
+import java.util.Iterator;
+
 public class MyLinkedList<T> implements MyList<T> {
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
     private class MyNode {
         T data;
         MyNode next;
@@ -27,34 +34,116 @@ public class MyLinkedList<T> implements MyList<T> {
     }
 
     @Override
-    public void remove(int index) {
-        if (index < 0 || index >= size)
-            throw new IndexOutOfBoundsException();
-        MyNode current = getNode(index);
+    public void set(int index, T item) {
+        checkIndex(index);
+        getNode(index).data = item;
+    }
 
-        if (current == head) {
-            head = current.next;
-            if (head != null) {
-                head.prev = null;
-            }
-        } else if (current == tail) {
-            tail = current.prev;
-            if (tail != null) {
-                tail.next = null;
-            }
-        } else {
-            current.prev.next = current.next;
-            current.next.prev = current.prev;
+    private void checkIndex(int index){
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + " not found");
         }
-        size--;
-        if (size == 0) {
+    }
+
+    @Override
+    public void add(int index, T element) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index);
+        }
+        if (index == 0) {
+            addFirst(element);
+        }
+        if (index == size) {
+            addLast(element);
+        }
+        MyNode current = getNode(index);
+        MyNode newNode = new MyNode(element);
+
+        newNode.prev = current.prev;
+        newNode.next = current;
+        current.prev.next = newNode;
+        current.prev = newNode;
+
+        size++;
+    }
+
+    @Override
+    public void addFirst(T element) {
+        
+    }
+
+    @Override
+    public void addLast(T element) {
+
+    }
+
+    @Override
+    public void removeFirst() {
+        if (head == null) {
+            throw new IndexOutOfBoundsException("The list is empty");
+        }
+//        T removedEl = head.data;
+        if (head == tail) {
             head = tail = null;
+        }else
+            head = head.next;
+        size--;
+    }
+    public void remove(int index) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void removeLast() {
+        if (head == null) {
+            throw new IndexOutOfBoundsException("The list is empty");
         }
+        if (head == tail) {
+            head = tail = null;
+        }else
+            tail = tail.prev;
+            tail.next = null;
+        size--;
+
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        return 0;
+    }
+
+//    @Override
+//    public boolean exists(Object object) {
+//        return false;
+//    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
     }
 
     @Override
     public T get(int index) {
         return getNode(index).data;
+    }
+
+    @Override
+    public T getFirst() {
+        return null;
+    }
+
+    @Override
+    public T getLast() {
+        return null;
     }
 
     private MyNode getNode(int index) {
